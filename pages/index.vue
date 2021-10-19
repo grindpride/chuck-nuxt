a6e0e8af167664aedf79059ca6b0d667c59855bd
v.koryanov <v.koryanov@etpgpb.ru>
2021-10-19 21:50:59 +0300

    nuxt3

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="container">
     <nav class="nav">
-      <div>
         <form @submit.prevent="search">
           <input v-model="query" type="text">
           <button>Search</button>
           <button @click="refresh">Refresh</button>
         </form>
-
-      </div>
     </nav>
     <Card :joke="joke.value"></Card>
   </div>
